0f183c99ebdde9a64ab0edde98aea3577dbb9dc0
Brekstyn <vasugounder6@gmail.com>
2025-01-12 01:31:38 +0530

    Pow(x,n)

---
+++ Arrays_3/Day14.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    public double pow(double x,  long n){
+        if(n == 0) return 1;
+        if(n < 0) return pow(1/x , -n);
+        if(n%2 == 0) return pow(x*x , n/2);
+        return x*pow(x*x,n/2);
+       
+    }
+    public double myPow(double x, int n) {
+      return pow(x,(long)n);
+
+    }
+}
