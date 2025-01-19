5da0ab64fdbdca296609cdee467e067518485d43
Brekstyn <vasugounder6@gmail.com>
2025-01-20 03:40:22 +0530

    4Sum

---
+++ Arrays_4/Day20.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList<List<Integer>> result  = new ArrayList<>();
+        
+        Arrays.sort(nums);
+
+        for(int i = 0;i<nums.length - 3;i++){
+
+            for(int j = i+1;j<nums.length -2;j++){
+
+                long startsum = nums[i]+nums[j];
+                long remainingsum = target - startsum;
+                int start = j+1;
+                int end = nums.length -1;
+                while(start < end){
+                    long sum = nums[start]+ nums[end];
+                    if( sum == remainingsum){
+                        ArrayList<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[start]);
+                        list.add(nums[end]);
+                        if(!result.contains(list)){
+                            result.add(list);
+                        }
+                        start++;
+                        end--;
+                    }
+                    else if(sum < remainingsum){
+
+                        start++;
+                    }
+                    else{
+                        end--;
+                    }
+                   
+                }
+            }
+
+        }
+    
+        return result;
+        
+    }
+}
