d57fe70befeb8bda106f33212b75a6728e7fe155
Brekstyn <vasugounder6@gmail.com>
2025-02-02 23:25:58 +0530

    Linked List

--- Arrays_4/Day20.java
+++ Arrays_4/Day20.java
@@ -1,45 +1,43 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        ArrayList<List<Integer>> result  = new ArrayList<>();
-        
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
 
-        for(int i = 0;i<nums.length - 3;i++){
+        for (int i = 0; i < nums.length - 3; i++) {
 
-            for(int j = i+1;j<nums.length -2;j++){
+            for (int j = i + 1; j < nums.length - 2; j++) {
 
-                long startsum = nums[i]+nums[j];
+                long startsum = nums[i] + nums[j];
                 long remainingsum = target - startsum;
-                int start = j+1;
-                int end = nums.length -1;
-                while(start < end){
-                    long sum = nums[start]+ nums[end];
-                    if( sum == remainingsum){
+                int start = j + 1;
+                int end = nums.length - 1;
+                while (start < end) {
+                    long sum = nums[start] + nums[end];
+                    if (sum == remainingsum) {
                         ArrayList<Integer> list = new ArrayList<>();
                         list.add(nums[i]);
                         list.add(nums[j]);
                         list.add(nums[start]);
                         list.add(nums[end]);
-                        if(!result.contains(list)){
+                        if (!result.contains(list)) {
                             result.add(list);
                         }
                         start++;
                         end--;
-                    }
-                    else if(sum < remainingsum){
+                    } else if (sum < remainingsum) {
 
                         start++;
-                    }
-                    else{
+                    } else {
                         end--;
                     }
-                   
+
                 }
             }
 
         }
-    
+
         return result;
-        
+
     }
 }
